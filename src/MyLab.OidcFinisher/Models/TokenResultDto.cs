58228354b35d96c226000f64d2bb1726d0609865
Andrey O. <ozzy.ext@gmail.com>
2024-08-07 14:19:22 +0300

    Fix bug with token response properties renaming

--- src/MyLab.OidcFinisher/Models/TokenResultDto.cs
+++ src/MyLab.OidcFinisher/Models/TokenResultDto.cs
@@ -1,13 +1,19 @@
 ﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace MyLab.OidcFinisher.Models
 {
     public class TokenResultDto
     {
+        [JsonProperty("access_token")]
         [JsonPropertyName("access_token")]
         public string? AccessToken { get; set; }
+
+        [JsonProperty("refresh_token")]
         [JsonPropertyName("refresh_token")]
         public string? RefreshToken { get; set; }
+
+        [JsonProperty("id_token")]
         [JsonPropertyName("id_token")]
         public string? IdToken { get; set; }
     }
